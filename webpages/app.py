7815362ce696c9450c1f5526c49beff7dc2e907f
Hector29 <114966508+Hnam29@users.noreply.github.com>
2025-05-27 15:07:49 +0700

    Update app.py

--- webpages/app.py
+++ webpages/app.py
@@ -7,7 +7,7 @@ from get_data_from_db import execute_sql_to_dataframe, execute_sql_ddl
 
 def main_app():
 
-    with open('/Users/vuhainam/Documents/PROJECT_DA/EdtechAgency/RANKING/2025/webpages/app.css')as f:
+    with open('styles/app.css')as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
 
     # sql_query = f"""
@@ -50,7 +50,7 @@ def main_app():
                 data = f.read()
             return base64.b64encode(data).decode()
         
-        img = get_img_as_base64('/Users/vuhainam/Documents/PROJECT_DA/EdtechAgency/Ranking/2025/webpages/bg.jpeg')
+        img = get_img_as_base64('webpages/bg.jpeg')
         page_bg_img = f"""
         <style>
             div[data-testid="stVerticalBlockBorderWrapper"]:nth-of-type(1) > div:nth-child(1) > div[data-testid="stVerticalBlock"] > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) {{
